83816366076c171359f855f083614e758ee83524
Thiago Silva Vilela <thiagosilvavilela@gmail.com>
2012-12-17 19:03:07 -0200

    Incluindo meu time

---
+++ teams/ThiagovTeam/config.py
@@ -0,0 +1,14 @@
+# Apenas caracteres alfa-numericos (seja criativo).
+TeamName = 'ThiagovTeam'
+
+# Filename.FactoryClassName (CASE-sensitive)
+AgentFactory = 'thiagovteam.ThiagovTeamFactory'
+
+# Nome completo dos alunos.
+Partners = ['Thiago1', 'Thiago2']
+
+# Parametros dos agentes (opcional).
+AgentArgs = {}
+
+# e-mails (opcional).
+NotifyList = ['fulano@silva.com', 'ciclano@soares.com']
